0bc8788eff8cc38094b54ecd176551dd1da0840f
Arun Reddy <gregorian21@yahoo.com>
2016-08-26 23:56:33 +0100

    Haskell Sieve

--- sieve/Haskell/Sieve.hs
+++ sieve/Haskell/Sieve.hs
@@ -1,4 +1,4 @@
--- Not a true Sieve of Eratosthenes
+-- Not a true Sieve of Eratosthenes, but generates primes nevertheless
 primes x = sieve [] [2..x]
-    where sieve ps []     = reverse ps
-          sieve ps (x:xs) = sieve (x:ps) (filter (\n -> n `mod` x /= 0) xs)
+     where sieve ps []     = reverse ps
+           sieve ps (x:xs) = sieve (x:ps) (filter (\n -> n `mod` x /= 0) xs)
