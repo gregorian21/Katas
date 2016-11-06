919d34cf420234a44ec4d73e98acf2925758b5d0
Arun Reddy <gregorian21@yahoo.com>
2016-11-06 17:32:22 +0000

    Updated Docs + Return value

--- pi-montecarlo/python/montecarlo_pi.py
+++ pi-montecarlo/python/montecarlo_pi.py
@@ -1,9 +1,26 @@
 #! /usr/bin/env python3
+"""Calculates π (Pi) using Monte Carlo approximation
+
+Usage:
+
+    python3 montecarlo_pi <iterations>
+"""
 import sys
 from math import sqrt
 from random import random
 
 def calculate_pi(iterations):
+    """Calculates π using Monte Carlo approximation. Takes random points within
+    a unit square and checks whether they fall inside or outside a unit quarter
+    circle circumscribed within the square.
+
+    Args:
+        iterations: The number of iterations of the algorithm. The greater the
+        number, the more accurate the estimate of π.
+
+    Returns:
+        An estimate for the value of π.
+    """
     inside_circle = 0
     for _ in range (0, iterations):
         x = random()
@@ -13,10 +30,11 @@ def calculate_pi(iterations):
             inside_circle += 1
 
     pi = 4 * inside_circle / iterations
-    print("Pi: {}".format(pi))
+    return pi
 
 def main():
-    calculate_pi(int(sys.argv[1]))
+    pi = calculate_pi(int(sys.argv[1]))
+    print("Pi: {}".format(pi))
 
 if __name__ == '__main__':
     main()
